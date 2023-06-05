4f0b4c0eda8345daa34b1ce483b2562d36a8a661
Mongke22 <ddd.ddd99.99@mail.ru>
2023-06-05 21:00:11 +0300

    тест сериализации

--- app/src/main/java/com/example/treeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/treeapp/presentation/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.treeapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.treeapp.R
+import com.example.treeapp.domain.MyNode
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val temp = MyNode(
+            name = "mainRoot",
+            parent = null,
+            children =  arrayListOf()
+        )
+        val str = Json.encodeToString(temp)
+        val result = Json.decodeFromString<MyNode>(str)
+
+        Log.i("serialization", str)
+
+        Log.i("serialization", result.name)
+
+        Log.i("serialization", result.toString())
+
+
     }
 }
---
+++ app/src/main/java/com/example/treeapp/presentation/MainViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.treeapp.presentation
+
+class MainViewModel {
+}
